52a03af6e8083e165af6e02717f05dfe3e5072ce
Rajat Srivastava <rajat.sedate@gmail.com>
2022-12-27 20:48:26 +0530

    Removed Key

--- dotnet-graphql-harperdb/Context/HarperConfiguration.cs
+++ dotnet-graphql-harperdb/Context/HarperConfiguration.cs
@@ -12,7 +12,7 @@ namespace GraphQL.Context
 			return new HarperDbConfiguration()
 			{
 				InstanceUrl = "https://graphql-dotnet-coredemo.harperdbcloud.com",
-				AuthToken = "YWRtaW46YWRtaW4=",
+				AuthToken = "=",
 				Schema = "graphql"
 			};
 		}
